0760016184db08678b168146ccb75a4ddbcaf52c
voyager <05170000805@ogrenci.ege.edu.tr>
2023-02-20 14:58:23 +0300

    Modules extended

--- 02 - Modules/app.js
+++ 02 - Modules/app.js
@@ -1,2 +1,4 @@
 let tutorial_module = require('./tutorial_module');
-console.log(tutorial_module.sum(3,5));
+console.log(tutorial_module.sum(3,6));
+console.log(tutorial_module.PI);
+console.log(new tutorial_module.someObject());
--- 02 - Modules/tutorial_module.js
+++ 02 - Modules/tutorial_module.js
@@ -1,1 +1,8 @@
 exports.sum = (num1,num2) => num1 + num2;
+
+exports.PI = 3.14;
+class someObject{ constructor(){
+    console.log("Objct Created");
+}}
+
+exports.someObject = someObject;
