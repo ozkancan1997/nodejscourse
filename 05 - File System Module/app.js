29f7a73cfc4721a6ff5d1eece33d448ff80e39ab
voyager <ozkan.can@aol.com>
2023-02-21 10:24:40 +0300

    Worked of filesystem module and its methods

---
+++ 05 - File System Module/app.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+//code asynchronous, works different every time
+
+// fs.writeFile('./example.txt', 'Example text for file creating',(err)=>{
+//     if(err) console.log(err);
+//     else{
+//         console.log("File created.");
+//         fs.readFile('./example.txt','utf8',(err,file)=>{
+//             if(err) console.log(err);
+//             else console.log(file);
+//         })
+//     }
+// });
+
+// fs.rename('./example.txt','./example_new.txt',(err)=>{
+//     if(err) console.log(err);
+//     else console.log("Succesfully renamed.");
+// })
+
+// fs.appendFile('./example_new.txt','\nAppended data.',(err)=>{
+//     if(err) console.log(err);
+//     else console.log("Successfully appended.");
+// })
+
+// fs.unlink('./example_new.txt',(err)=>{
+//     if(err) console.log(err);
+//     else console.log("Successfully deleted file");
+// })
+
+
